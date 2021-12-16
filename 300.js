acd976080be692a80b2706ea715ef81e9aefb340
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 20:57:19 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'd648397c2f4ceb2049e9dbaf98cf46ac',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://wagerprocuratorantiterrorist.com/d648397c2f4ceb2049e9dbaf98cf46ac/invoke.js"></scr' + 'ipt>');
