feffc7845f95a7f73072f8565190e9e509b8e3ad
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 21:03:09 +0700

    Create 728.js

---
+++ 728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'cf499c4a78c0296a203adcac3d025397',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://wagerprocuratorantiterrorist.com/cf499c4a78c0296a203adcac3d025397/invoke.js"></scr' + 'ipt>');
