1c1f1ff6b22e19e48badd2e75f274cd992243de5
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 21:03:27 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c5b69fadf511d6654a7370dc0a3c7c81',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://wagerprocuratorantiterrorist.com/c5b69fadf511d6654a7370dc0a3c7c81/invoke.js"></scr' + 'ipt>');
